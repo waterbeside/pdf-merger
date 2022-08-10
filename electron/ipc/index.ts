9b1c8827bda20a8a9ab99dfe3de9dae48682ecfc
Natron Kan <waterbeside@163.com>
2022-08-10 20:45:26 +0800

    feat: merge-pdf handle

--- electron/ipc/index.ts
+++ electron/ipc/index.ts
@@ -1,5 +1,5 @@
 import { dialog, ipcMain } from 'electron'
-
+import {store} from './setIpcStore'
 
 export function setIpcs() {
   // 打开选择文件夹
@@ -10,5 +10,10 @@ export function setIpcs() {
     return res
   })
 
+  ipcMain.handle('merge-pdf', async (e: any, setting: any) => {
+    const fileName = setting.fileName
+    const fileList = await store.get('file-list')
+    const savedDir = await store.get('saved-dir')
+  })
 
 }
