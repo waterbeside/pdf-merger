9e4bbc2d847354264437ee0a78abc1dea394f04d
Natron Kan <waterbeside@163.com>
2022-08-10 12:18:53 +0800

    feat: 完善文件列表

--- src/App.vue
+++ src/App.vue
@@ -3,21 +3,55 @@
 // Check out https://vuejs.org/api/sfc-script-setup.html#script-setup
 // import HelloWorld from './components/HelloWorld.vue'
 import ipcStore from './utils/ipcStore'
-import { NUpload, NUploadDragger, NButton, NIcon } from 'naive-ui'
+import { ipcRenderer } from 'electron'
+import { NUpload, NUploadDragger, NButton, NIcon, NInput, NMessageProvider, useMessage } from 'naive-ui'
+import type { UploadFileInfo, UploadInst } from 'naive-ui'
 import SelectDirBtn from './components/SelectDirBtn/index.vue'
 import { Save20Regular, FolderOpen16Filled } from '@vicons/fluent'
-import { ref } from 'vue'
+import { ref, reactive } from 'vue'
+import PdfListItem from './components/PdfListItem/index.vue'
+import MessageApi from './components/MessageApi.vue'
 
 
+    
+
+const fileListRt = ref<UploadFileInfo[]>([])
+const uploadRef = ref<UploadInst | null>(null)
 const savedDir = ref(localStorage.getItem('saved-dir') || '')
 const defaultSavedDir = localStorage.getItem('saved-dir')
+const saveFileName = ref<string>('')
+
 if (defaultSavedDir) {
   ipcStore('saved-dir').set(defaultSavedDir)
 }
 
 // 上传pdf文件
 const uploadChange = function(options: any) {
-  console.log(options)
+  const { file, fileList  } = options
+  const oldList = fileListRt.value
+  // let reader = new window.FileReader()
+  // re
+  const isInList = oldList.some((item: any) => item.file.path === file.file.path)
+  if (isInList) {
+    window.$message.warning(`${file.name} 已经存在于列表中`)
+
+    uploadRef.value?.clear()
+    return
+  }
+  if (file.type === 'application/pdf') {
+    if (oldList.length === 0 || saveFileName.value === '') {
+      saveFileName.value = 'merged_' + file.name
+    }
+    oldList.push(file)
+    fileListRt.value = oldList
+    ipcStore('file-list').set(fileListRt.value.map((item: UploadFileInfo) => ({
+      name: item.name,
+      path: item.file?.path,
+      id: item.id
+    })))
+  } else {
+    window.$message.error(`${file.name} 不是一个有效的pdf文件`)
+  }
 }
 // 选择保存的目标目录
 const selectSavedDir = function(res: any) {
@@ -25,24 +59,49 @@ const selectSavedDir = function(res: any) {
     ipcStore('saved-dir').set(res.filePaths[0])
     savedDir.value = res.filePaths[0]
     localStorage.setItem('saved-dir', res.filePaths[0])
-  }
-  
+  } 
 }
+
+// 移除文件
+const removeItem = function(fileData: UploadFileInfo) {
+  fileListRt.value = fileListRt.value.filter(item => item.id !== fileData.id)
+  ipcStore('file-list').set(fileListRt.value.map((item: UploadFileInfo) => ({
+    name: item.name,
+    path: item.file?.path,
+    id: item.id
+  })))
+}
+
+// 合并文件（点击保存）
+const save = function() {
+
+}
+
 
 </script>
 
 <template>
+<n-message-provider>
+  <MessageApi/>
+</n-message-provider>
   <div class="top-bar">
-    <div class="saved-dir">
-      <SelectDirBtn class="saved-dir__btn" @after-selected="selectSavedDir">
-        <template #icon>
-          <n-icon class="select-dir-btn__icon">
-            <FolderOpen16Filled />
-          </n-icon>
-        </template>
-        保存到：<input class="saved-dir__input" :value="savedDir" readonly/>
-      </SelectDirBtn>
+    <div class="top-bar__left">
+      <div class="saved-dir">
+        <SelectDirBtn class="saved-dir__btn" @after-selected="selectSavedDir">
+          <template #icon>
+            <n-icon class="select-dir-btn__icon">
+              <FolderOpen16Filled />
+            </n-icon>
+          </template>
+          保存到：<input class="saved-dir__input" :value="savedDir" readonly/>
+        </SelectDirBtn>
+      </div>
+      <div class="sp"> / </div>
+      <div class="saved-name">
+        <n-input v-model:value="saveFileName" round clearable placeholder="请输入文件名" />
+      </div>
     </div>
+
     <div class="saved-btn">
       <n-button type="info" round>
         <template #icon>
@@ -55,29 +114,27 @@ const selectSavedDir = function(res: any) {
   <NUpload
     class="upload-box"
     :multiple="true"
+    directory-dnd
     accept="application/pdf"
     @change="uploadChange"
+    ref="uploadRef"
   >
     <NUploadDragger class="upload-box__inner">
       <div class="tips">
       <p>Drag files here</p>
       <p>拖动文件到此处，或点击添加。</p>
       </div>
+      <ul v-if="fileListRt.length > 0" class="file-list">
+        <template v-for="item in fileListRt" :key="item.batchId">
+          <pdf-list-item
+            :data="item"
+            @delete="removeItem"
+          >
+          </pdf-list-item>
+        </template>
+      </ul>
     </NUploadDragger>
   </NUpload>
-<!--   
-  <div class="logo-box">
-    <img style="height:140px;" src="./assets/electron.png" >
-    <span/>
-    <img style="height:140px;" src="./assets/vite.svg" >
-    <span/>
-    <img style="height:140px;" src="./assets/vue.png" >
-  </div>
-  <HelloWorld msg="Hello Vue 3 + TypeScript + Vite" />
-  <div class="static-public">
-    Place static files into the <code>/public</code> folder
-    <img style="width:77px;" :src="'./node.png'" >
-  </div> -->
 </template>
 
 <style lang="scss" scoped>
@@ -106,6 +163,17 @@ body {
   z-index:  100;
   background-color: #2c3034;
   box-shadow: 0 10px 10px rgba(0, 0, 0, 0.1);
+  color: #fff;
+  &__left {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: flex-start;
+    flex: 1;
+    .sp {
+      padding: 0 12px;
+    }
+  }
   
 }
 .saved-dir {
@@ -117,6 +185,9 @@ body {
     border: none;
     min-width: 200px;
   }
+}
+
+.saved-name {
 }
 
 .save-btn {
@@ -137,17 +208,27 @@ body {
   }
   
   &__inner {
+    padding-top: $topbar-height + 20px;
     display: flex;
-    justify-content: center;
+    flex-direction: column;
+    align-items: center;
     flex-grow: 1;
     background-color: #292c32;
     border: none;
+    color: #fff;
     .tips {
       padding: 20px;
       font-size: 14px;
       color: #999;
+      line-height: 1.2;
     }
-
+    .file-list {
+      width: 100%;
+      &__item {
+        float: left;
+        width: 120px;
+      }
+    }
   }
 }
 
